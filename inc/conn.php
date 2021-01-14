fe79b79ea862c87c063f93c345009f89cb3aedbe
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-14 19:25:57 +0530

    created connection file

--- inc/conn.php
+++ inc/conn.php
@@ -1,5 +1,17 @@
 <? php
 
+	$host='localhost';
+	$user='root';
+	$password='';
+	$db='ums';
+	$con=mysqli_connect('localhost','root','','ums');
+
+	//checking connection
+	if(mysqli_connect_errno()){
+		die('Database connection failed'.mysqli_connect_errno());
+	}else{
+		echo "can't Access";
+	}
 
 
 
