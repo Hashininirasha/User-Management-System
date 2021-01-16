7ce770a65d6db460a7ec0226a3b1aa6f6251289f
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-16 12:00:13 +0530

    added css file and import

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <head>
 	
 	<title>User Management System</title>
+  <link rel="stylesheet" href="css.css" >
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
