1e08b7b97df18f8e21985f981132b5fce34a3070
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-20 21:56:36 +0530

    session-destory

--- logout.php
+++ logout.php
@@ -1,9 +1,17 @@
 <?php
 
+session_start();
+$_SESSION=array():
+
+//erase cookey
+if(isset($_COOKIE[session_name()])){
+	setcookie(session_name(), " ", time()-86400, '/');
+
+}
 
 
-
-
+//session destory
+	session_destory();
 
 
 
