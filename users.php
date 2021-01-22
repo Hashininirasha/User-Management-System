146723f9e7ad8d907a940c3293a68b7cae93cf2d
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-22 23:45:15 +0530

    added table

--- users.php
+++ users.php
@@ -58,6 +58,38 @@
 
  </div>
  <h1>Users <span><a href="add-user.php"> Add New User </a></span></h1>
+ <table class="table">
+  <thead>
+    <tr class="table table-sm table-dark">
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name </th>
+      <th scope="col">Last login</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr >
+      <th scope="row">3</th>
+      <td >Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
         </div>
 
 
